ddc0024fac7c30bedb489decaea1efd5c2a23ee7
Alexander White <whitie@whitie.ru>
2023-03-10 00:48:25 +0300

    new file:   provider/games/zinogor/css/style.css 	modified:   provider/games/zinogor/index.html 	new file:   provider/games/zinogor/static/binoculars.png 	new file:   provider/games/zinogor/static/rocket_01.png 	new file:   provider/games/zinogor/static/rocket_02.png new file:   provider/games/zinogor/static/rocket_03.png new file:   provider/games/zinogor/static/sky.png 	modified:   provider/ws_server/index.js

--- provider/ws_server/index.js
+++ provider/ws_server/index.js
@@ -9,18 +9,47 @@ const server = createServer({
 
 const wss = new WebSocketServer({ server })
 
-wss.on('connection', ws => {
+wss.on('connection', (ws) => {
   ws.on('error', console.error)
 
-  ws.on('message', data => {
+  ws.on('message', (data) => {
     console.log('Message received: %s', data)
 
-    wss.clients.forEach(client => {
-      client.send(JSON.stringify({test:123}))
-    })
+    // wss.clients.forEach(client => {
+    //   client.send(JSON.stringify({test:123}))
+    // })
   })
 
   ws.send('Connected to WS. This is a test message.')
 })
+
+class RocketLaunch {
+  launch_states = {
+    close: 100,
+    mid: 200,
+    far: 300,
+    heating: 400,
+    launch: 500,
+    takeoff: 600,
+    blow: 700,
+    ready: 800
+  }
+  launch_count = 10
+  launch_interval = null
+
+  constructor() {
+    this.launch_interval = setInterval(this.change_state, 1000)
+  }
+
+  change_state() {
+    if (this.launch_count > 8) {
+      wss.clients.forEach((client) => {
+        client.send(JSON.stringify({ this.launch_count, this.launch_states.close}))
+      })
+    }
+  }
+}
+
+let RocketLaunch = new RocketLaunch()
 
 server.listen(9000)
