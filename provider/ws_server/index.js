4969cc864ecedef1a98e28a50527de2405f6f094
Alexander White <whitie@whitie.ru>
2023-03-11 00:13:25 +0300

    modified:   provider/ws_server/index.js

--- provider/ws_server/index.js
+++ provider/ws_server/index.js
@@ -1,6 +1,7 @@
 const { createServer } = require('https')
 const { readFileSync } = require('fs')
 const { WebSocketServer } = require('ws')
+const { randomBytes } = require('crypto')
 
 const server = createServer({
   cert: readFileSync('/etc/letsencrypt/live/wsup.whitie.ru/cert.pem'),
@@ -34,22 +35,85 @@ class RocketLaunch {
     blow: 700,
     ready: 800
   }
-  launch_count = 10
+  launch_countdown = 10
   launch_interval = null
+  flight_duration = null
+  flight_interval = null
 
   constructor() {
     this.launch_interval = setInterval(this.change_state, 1000)
   }
 
   change_state() {
-    if (this.launch_count > 8) {
-      wss.clients.forEach((client) => {
-        client.send(JSON.stringify({ this.launch_count, this.launch_states.close}))
-      })
+    switch (this.launch_count--) {
+      case (10, 9):
+        wss.clients.forEach((client) => {
+          client.send(
+            JSON.stringify({
+              launch_countdown: this.launch_countdown,
+              launch_states: this.launch_states.close
+            })
+          )
+        })
+        break
+      case (8, 7):
+        wss.clients.forEach((client) => {
+          client.send(
+            JSON.stringify({
+              launch_countdown: this.launch_countdown,
+              launch_states: this.launch_states.mid
+            })
+          )
+        })
+        break
+      case (6, 5):
+        wss.clients.forEach((client) => {
+          client.send(
+            JSON.stringify({
+              launch_countdown: this.launch_countdown,
+              launch_states: this.launch_states.far
+            })
+          )
+        })
+        break
+      case (4, 3, 2, 1):
+        wss.clients.forEach((client) => {
+          client.send(
+            JSON.stringify({
+              launch_countdown: this.launch_countdown,
+              launch_states: this.launch_states.launch
+            })
+          )
+        })
+        break
+      case 0:
+        wss.clients.forEach((client) => {
+          client.send(
+            JSON.stringify({
+              launch_countdown: this.launch_countdown,
+              launch_states: this.launch_states.takeoff
+            })
+          )
+          this.start_flight()
+        })
+        break
     }
+  }
+  start_flight() {
+    clearInterval(this.launch_interval)
+    flight_duration = this.generate_duration()
+  }
+  generate_duration() {
+    let random = this.get_random(4) + this.get_random(4, 5)+ this.get_random(4, 5)+ this.get_random(4, 5)+ this.get_random(8, 9)
+    random = random < 0.1 ? 0.1 : random
+    return random
+  }
+  get_random(max = 1, chance = 0) {
+    let new_random = Math.round(Math.random() * 10) >= chance ? Math.random() * max : 0
+    return new_random
   }
 }
 
-let RocketLaunch = new RocketLaunch()
+let rocketLaunch = new RocketLaunch()
 
 server.listen(9000)
